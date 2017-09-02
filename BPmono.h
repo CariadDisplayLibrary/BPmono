3202a1d9a89728a8da4d7e910f5643ded8b76b27
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:35:28 +0100

    Initial import

---
+++ BPmono.h
@@ -0,0 +1,29 @@
+#ifndef _BPMONO_H
+#define _BPMONO_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t BPmono10[];
+    extern const uint8_t BPmono11[];
+    extern const uint8_t BPmono12[];
+    extern const uint8_t BPmono13[];
+    extern const uint8_t BPmono14[];
+    extern const uint8_t BPmono15[];
+    extern const uint8_t BPmono16[];
+    extern const uint8_t BPmono17[];
+    extern const uint8_t BPmono18[];
+    extern const uint8_t BPmono19[];
+    extern const uint8_t BPmono20[];
+    extern const uint8_t BPmono21[];
+    extern const uint8_t BPmono22[];
+    extern const uint8_t BPmono23[];
+    extern const uint8_t BPmono24[];
+    extern const uint8_t BPmono25[];
+    extern const uint8_t BPmono26[];
+    extern const uint8_t BPmono27[];
+    extern const uint8_t BPmono28[];
+    extern const uint8_t BPmono29[];
+    extern const uint8_t BPmono30[];
+};
+#endif
